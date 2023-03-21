38909695313fa65198e53d3bafaf4e0a64bef952
NicoleHitter <nicole.hitter92@gmail.com>
2023-03-21 20:16:05 +0000

    Adding items and editing them

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,18 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+if os.path.isfile('env.py'):
+    import env
+    
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f@k#-=q&)q#w8ouin^m16zsxvkp2qq(dz^@5+nr(npzm)udxa@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
